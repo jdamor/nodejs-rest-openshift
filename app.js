b8180473d283b355e9d1082572c0bab1cb6bc9af
jdamor <jdamor.juampi@gmail.com>
2016-04-02 20:16:34 +0200

    Update Nodejs latest cartdrige

---
+++ app.js
@@ -0,0 +1,24 @@
+const http         = require('http'),
+      fs           = require('fs'),
+      path         = require('path'),
+      contentTypes = require('./utils/content-types'),
+      sysInfo      = require('./utils/sys-info'),
+      express      = require('express'),
+      env          = process.env;
+
+let app = express();
+//app.use('/', express.static(__dirname + '/static'));
+
+app.get('/health', function (req, res) {
+   res.sendStatus(200);     
+});
+app.get('/asciimo', function(req, res) {
+   var link = "http://i.imgur.com/kmbjB.png";
+   res.send("<html><body><img src='" + link + "'></body></html>");    
+});
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
+
+
